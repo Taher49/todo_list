d867e3bdf901721a41146ba8fcce74ed573c3e40
Taher49 <taherelbouhtouri49@gmail.com>
2024-04-04 18:00:02 +0000

    fix some errors

--- src/components/footer.js
+++ src/components/footer.js
@@ -14,14 +14,28 @@ export default function Footer () {
     setTodos(newTodos)
     setCheckAll(!CheckAll)
   }
+
+  const deleteTodo = () =>{
+    const newTodos = todos.filter(todo =>{
+      return todo.complete === false
+    })
+    setTodos(newTodos)
+    setCheckAll(false)
+  }
     return (
-        <div className="row">
-        <label htmlFor="all" >
-        <input type="checkbox" name="all" id="all" />
-        ALL
-        </label>
-        <p>You have 0 to do</p>
-        <button id="delete">Delete</button>
-      </div>
+      <>
+      {
+            todos.length === 0 ? <h2> congratulations! Noyhings To Do</h2>
+           :  <div className="row">
+             <label htmlFor="all" >
+             <input type="checkbox" name="all" id="all" 
+              onClick={handleCheckAll} checked={CheckAll}  />
+             ALL
+             </label>
+             <p>You have {todos.filter(todo => todo.complete === false ).length} to do</p>
+             <button id="delete" onClick={deleteTodo}>Delete</button>
+           </div>
+      }
+      </>
     )
 }
--- src/components/forminput.js
+++ src/components/forminput.js
@@ -1,20 +1,25 @@
-import { useContext, useState } from "react"
+import { useContext, useEffect, useRef, useState } from "react"
 import { DataContext } from "./dataProvider"
 
 export default function Forminput () {
     const [todos, setTodos] = useContext(DataContext);
     const [ todoName, setTodoName] = useState('');
+    const todoInput = useRef();
 
     const addTodo = e =>{
         e.preventDefault();
         setTodos([...todos, {name: todoName, complete: false}])
         setTodoName ('');
+        todoInput.current.focus();
     }
 
+    useEffect(()=> {
+        todoInput.current.focus();
+    },[])
 
      return (
         <form autoComplete="off" onSubmit={addTodo}>
-        <input type="text" name="todos" id="todos"
+        <input type="text" name="todos" id="todos" ref={todoInput}
         required placeholder="what needs to be done ?"
         value={todoName} onChange={e => setTodoName(e.target.value.toLowerCase())}
         />
